ae0c2a7364dbb7ca92c3c914c632929b85c29228
Ralph Sablaon <rsablaon88@gmail.com>
2019-07-22 20:47:23 -0700

    js added

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-$(document)
+$(document).ready(function() {
+
+});
+
+function search() {
+	// Get Search Values
+
+	// Build Request
+
+		// then Update UI with articles
+}
+
+function clearResults() {
+	// Empty articleList
+}
